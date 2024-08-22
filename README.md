ed4242c786730e80f40512bd06fd76b410ba0853
Mike <grabbou@gmail.com>
2024-08-22 20:25:31 +0400

    update

--- README.md
+++ README.md
@@ -1,12 +1,7 @@
-Playground Template
+RUC 2024 Conference Demo Server
 =====
 
-Welcome to Rise!
-
-To start your server, run:
-
 ```sh
+$ npm run tunnel
 $ npm run dev
 ```
-
-This project includes all of Rise Kit and works inside Rise Playground out of the box. To learn more, [check out our docs!](https://rise.tools/docs/playground/#building-for-playground)
--- src/models.tsx
+++ src/models.tsx
@@ -1,3 +1,4 @@
+import { navigate } from "@rise-tools/kit-expo-router/server";
 import {
   VenueInfo,
   LiveStreamInfo,
@@ -6,11 +7,18 @@ import {
   BuildDetails,
   OrganizersInfo,
   PoweredByExpo,
+  InfoSection,
+  Button,
+  ThemedText,
 } from "../../ruc2024-mobile-app/rise/components/server";
+import { Text } from "@rise-tools/kit-react-native/server";
 
 function Info() {
   return (
     <>
+      <InfoSection title="Rise Tools">
+        <Button onPress={navigate('about')} title="Learn more" />
+      </InfoSection>
       <VenueInfo />
       <LiveStreamInfo />
       <DiscordInfo />
@@ -24,4 +32,5 @@ function Info() {
 
 export const models = {
   info: Info,
+  about: () => <InfoSection title="About" />,
 }
