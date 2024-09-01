ec82dae9ad655e830dd93df0fb782fc120a6e480
Mike <grabbou@gmail.com>
2024-09-01 18:28:50 +0200

    save

--- .eslintrc.js
+++ .eslintrc.js
@@ -42,6 +42,7 @@ module.exports = {
       },
       rules: {
         '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
         '@typescript-eslint/ban-ts-comment': 'off',
         '@typescript-eslint/ban-ts-ignore': 'off',
         '@typescript-eslint/explicit-module-boundary-types': 'off',
--- src/client.tsx
+++ src/client.tsx
@@ -1,13 +1,3 @@
-/**
- * React Universe has available the following modules:
- * - kit-expo-router
- * - kit-linking
- * - kit-react-native
- * - kit-expo-video
- *
- * Importing anything else (from kitchen-sink or kit-react-navigation) will trigger an error.
- */
-
 import { Video } from '@prisma/client'
 import { navigate, StackScreen } from '@rise-tools/kit-expo-router/server'
 import { openURL } from '@rise-tools/kit-linking/server'
@@ -39,83 +29,10 @@ import { edition } from './admin'
 const $lg = 20
 const $xl = 30
 
-function BronzeSponsor() {
-  return (
-    <View style={{ flexDirection: 'column', alignItems: 'center', gap: $lg }}>
-      <ThemedText fontSize={24}>Bronze</ThemedText>
-      <Image
-        source={{ uri: 'https://avatars.githubusercontent.com/u/167453825?s=200&v=4' }}
-        style={{ width: 100, height: 100 }}
-      />
-    </View>
-  )
-}
-
-function ArchiveInfo() {
-  return (
-    <InfoSection title="Talk Archive">
-      <ThemedText style={{ marginBottom: 20 }}>
-        Have you missed past editions? Worry not. We got videos for you!
-      </ThemedText>
-      <Button title="Browse" onPress={navigate('archive')} />
-    </InfoSection>
-  )
-}
-
-function VideoSection({ title, content }: { title: string; content?: Video[] }) {
-  const data = content?.map((item) => ({
-    key: item.id,
-    view: (
-      <TouchableOpacity onPress={openURL(`https://youtube.com/watch?v=${item.id}`)}>
-        <Image
-          source={{ uri: item.thumbnail }}
-          style={{ width: 250, height: 150, marginLeft: $lg }}
-        />
-      </TouchableOpacity>
-    ),
-  }))
-
-  if (!data) {
-    return <ActivityIndicator />
-  }
-
-  if (data.length === 0) {
-    return null
-  }
-
-  return (
-    <>
-      <ThemedText style={{ fontSize: $xl, padding: $lg }}>{title}</ThemedText>
-      <FlatList horizontal showsHorizontalScrollIndicator={false} data={data} />
-    </>
-  )
-}
-
-const Archive = view((get) => {
-  return (
-    <>
-      <StackScreen options={{ title: 'Archive', headerBackTitle: 'Go back' }} />
-      <ScrollView contentContainerStyle={{ gap: 0, paddingBottom: $xl }}>
-        <VideoSection title="2023" content={get(edition.get('2023')!)} />
-        <VideoSection title="2022" content={get(edition.get('2022')!)} />
-        <VideoSection title="2021" content={get(edition.get('2021')!)} />
-        <VideoSection title="2020" content={get(edition.get('2020')!)} />
-        <VideoSection title="2019" content={get(edition.get('2019')!)} />
-        <VideoSection title="2018" content={get(edition.get('2018')!)} />
-        <VideoSection title="2017" content={get(edition.get('2017')!)} />
-      </ScrollView>
-    </>
-  )
-})
-
 function Info() {
   return (
     <>
-      {/* This should be removed at the start of the demo */}
-      <ArchiveInfo />
       <SponsorsInfo />
-      {/* This should be removed at the start of the demo */}
-      <BronzeSponsor />
       <VenueInfo />
       <LiveStreamInfo />
       <DiscordInfo />
@@ -128,5 +45,4 @@ function Info() {
 
 export const models = {
   info: Info,
-  archive: Archive,
 }
